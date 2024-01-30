31c5d4824f6b174845b7bd348827dccc8b7a829c
Aniruth Ramesh <anikrishna3@gmail.com>
2024-01-30 13:25:54 -0500

    completed class activities

--- quiz/scripts/closure.js
+++ quiz/scripts/closure.js
@@ -4,10 +4,12 @@ function setColor(set) {
             let userColor = document.getElementById('color').value;
             document.getElementById('myPara').style.color = userColor;
         }
-        
+
 }
 
 window.onload = function() {
     let toggle = true;
-    document.getElementById('btn').onclick = setColor(toggle);
+    document.getElementById('btn').onclick = function (){
+        setColor(toggle)
+    };
 }
--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,19 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+
+function logMsg(){
+  console.log(`error message ${this.errMsg}`)
+}
+
+const e1 = {
+  errMsg : "this is error 1"
+}
+
+const e2 = {
+  errMsg : "this is error 2"
+}
+
+logMsg.call(e1)
+logMsg.call(e2)
+
+
+
