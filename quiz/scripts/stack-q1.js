f8b8ce3799632d06d1c41612e679d0a60e35b88b
Aniruth Ramesh <anikrishna3@gmail.com>
2024-02-06 13:00:50 -0500

    finished class activities

--- quiz/scripts/stack-q1.js
+++ quiz/scripts/stack-q1.js
@@ -16,15 +16,26 @@ class PStackImpl extends PStack {
     super();
   }
 
+  set persons(persons){
+    this._persons = persons
+  }
+
+  get persons(){
+    return this._persons
+  }
+
   push(p) {
     return this._persons.push(p)
   }
 
   pop() {
-    return this._persons.pop().age
+    if(this._persons.length) return this._persons.pop().age
+    else return "empty"
   }
 }
 
+//the reason initially we get the error is because, when we do pstack.persons -> it will create a new object instead of accessing the _persons.
+//so the _person contains only one element
 let pstack = new PStackImpl();
 pstack.persons = [{name: 'Jojo', age: 21}, {name: 'Gabi', age: 29}]
 pstack.push({name: 'Dein', age: 19});
--- scripts/module-demo.js
+++ scripts/module-demo.js
@@ -6,3 +6,8 @@ import { add, multiply } from './math.js';
 // Using the imported functions
 console.log(add(5, 3)); // Output: 8
 console.log(multiply(2, 4)); // Output: 8
+
+
+//we need to load the HTML and JS from same source or else we will get a CORS error when using module type with HTML
+//if i dont use Live server, ill get this error.
+//we can create a server and bundle them together. Live server automatically does this
